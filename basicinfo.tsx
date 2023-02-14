aa98e2ca182c208a776605662f9d2de73b6a2d41
Mbiet <mbietuko@udel.edu>
2023-02-14 16:37:06 -0500

    uncommented and edited .tsx file

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -1,13 +1,8 @@
-/* This is a basic file we are using to familiarize ourselves with you! Simply un-comment these
-below variables and update them with stuff about you. Yeah... I know... the classic basic info
-everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
-/*Un-comment this and fill in the info
-const yourName : String = "Your name";
-let yourPronouns : String = "Pro/Nouns";
-const yourMajor : String = "Your major";
-let classification : String = "freshman/soph/junior/senior/supersenior";
-let oneExcite : String = "One thing you're excited about in this course";
-let anyConcerns : String = "If anything, list what you may be worried about in this course";
-let funFact : String = "And the classic, list one fun fact about yourself";
-*/
+const yourName : String = "Mbiet Uko";
+let yourPronouns : String = "N/A";
+const yourMajor : String = "Computer Science";
+let classification : String = "senior";
+let oneExcite : String = "I get more hands on assignments";
+let anyConcerns : String = "N/A";
+let funFact : String = "I love Soccer";
